6ff0eb032371356f5adcbb1d4e223d2ff7ebd8b9
Nuriddin <nmuhammadjanov@gmail.com>
2021-10-30 15:35:09 +0500

    fixes

--- handlers/back.js
+++ handlers/back.js
@@ -3,47 +3,55 @@ const replies = require('../replies');
 const catchAsync = require('../utils/catchAsync');
 
 module.exports = catchAsync(async (ctx, next) => {
-    const { session } = ctx;
+	const { session } = ctx;
 
-    switch (session.menu.back) {
-        case menu.home: {
-            ctx.session.menu = {
-                current: menu.home,
-                back: null,
-            };
-            return replies.home(ctx, next);
-        }
+	switch (session.menu.back) {
+		case menu.home: {
+			ctx.session.menu = {
+				current: menu.home,
+				back: null,
+			};
+			return replies.home(ctx, next);
+		}
 
-        case menu.packages: {
-            ctx.session.menu = {
-                current: menu.packages,
-                back: menu.home,
-            };
-            return replies.packages(ctx, next);
-        }
+		case menu.packages: {
+			ctx.session.menu = {
+				current: menu.packages,
+				back: menu.home,
+			};
+			return replies.packages(ctx, next);
+		}
 
-        case menu.products: {
-            ctx.session.menu = {
-                current: menu.products,
-                back: menu.packages,
-            };
-            return replies.products(ctx, next);
-        }
+		case menu.products: {
+			ctx.session.menu = {
+				current: menu.products,
+				back: menu.packages,
+			};
+			return replies.products(ctx, next);
+		}
 
-        case menu.product: {
-            ctx.session.menu = {
-                current: menu.product,
-                back: menu.products,
-            };
-            return replies.product(ctx, next);
-        }
+		case menu.product: {
+			ctx.session.menu = {
+				current: menu.product,
+				back: menu.products,
+			};
+			return replies.product(ctx, next);
+		}
 
-        case menu.settings: {
-            ctx.session.menu = {
-                current: menu.settings,
-                back: menu.home,
-            };
-            return replies.settings(ctx, next);
-        }
-    }
+		case menu.settings: {
+			ctx.session.menu = {
+				current: menu.settings,
+				back: menu.home,
+			};
+			return replies.settings(ctx, next);
+		}
+
+		case menu.basket: {
+			ctx.session.menu = {
+				current: menu.basket,
+				back: menu.home,
+			};
+			return replies.basket(ctx, next);
+		}
+	}
 });
--- replies/product.js
+++ replies/product.js
@@ -4,34 +4,37 @@ const { generateCaption } = require('../utils/lazy');
 const packages = require('../constants/packages');
 
 module.exports = async (ctx, next) => {
-    const {
-        replyWithPhoto,
-        reply,
-        i18n,
-        session: { user, package, productname },
-    } = ctx;
+	const {
+		replyWithPhoto,
+		reply,
+		i18n,
+		session: { user, package, productname },
+	} = ctx;
 
-    const [product] = await Products.getByName(
-        productname,
-        user.language,
-        package
-    );
+	const [product] = await Products.getByName(
+		productname,
+		user.language,
+		package
+	);
 
-    if (product) {
-        const { price, image } = product;
+	if (product) {
+		const { price, image } = product;
 
-        const caption = generateCaption(
-            i18n,
-            product,
-            price,
-            user.language,
-            package === packages.pouch
-        );
+		const caption = generateCaption(
+			i18n,
+			product,
+			price,
+			user.language,
+			package === packages.pouch
+		);
 
-        ctx.session.product = product;
+		ctx.session.product = product;
 
-        replyWithPhoto(image.url, keyboard.product(i18n, package, caption));
-        reply(i18n.t('choose_amount'), { parse_mode: 'markdown' });
-        next();
-    }
+		await replyWithPhoto(
+			image.url,
+			keyboard.product(i18n, package, caption)
+		);
+		reply(i18n.t('choose_amount'), { parse_mode: 'markdown' });
+		next();
+	}
 };
